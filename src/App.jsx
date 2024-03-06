9d3d3bb743a321a00d3d1d3bc609811e46123254
Viktoriia Kostak <viktoriakostak570@gmail.com>
2024-03-06 09:27:48 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Link, Routes }
     from "react-router-dom";
 import Amenities from './pages/Amenities/amenities';
 function App() {
-  
+  //test
   return (
     <>
       <Router>
